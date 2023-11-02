4947efbd7aeefd2285019317508bb7c539f77dc7
Maryia Piarshai <mary.pershay@gmail.com>
2023-11-02 16:36:32 +0100

    refactor: search bar to functional component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,101 +1,78 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import '../assets/styles/SearchBar.css';
 import { PersonInfo } from '../interfaces/interfaces';
 
-type SearchBarState = {
-  inputValue?: string;
-  results?: [PersonInfo];
-  isLoading?: boolean;
-  hasError?: boolean;
-};
+export default function SearchBar(props) {
+  const [inputValue, setInputValue] = useState('');
+  const [, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
 
-type SearchBarProps = {
-  updateResults: (value: []) => void;
-};
+  const { setSearchTerm } = props;
 
-class SearchBar extends Component<SearchBarProps, SearchBarState> {
-  state = {
-    inputValue: '',
-    results: [],
-    isLoading: false,
-    hasError: false,
-  };
-
-  constructor(props: SearchBarProps) {
-    super(props);
-    this.setState = this.setState.bind(this);
-  }
-
-  fetchData(value) {
+  const fetchData = (value) => {
     const query = value.toLowerCase();
-    this.setState({ isLoading: true });
+    setIsLoading(true);
     const url: string = 'https://swapi.dev/api/people/';
     const endPoint: string = query ? url + '?search=' + query : url;
     fetch(endPoint)
       .then((response) => response.json())
       .then((json) => {
-        this.setState({ results: json.results });
-        this.setState({ isLoading: false });
-        this.setInitialValue(query);
-        this.props.updateResults(json.results);
+        setResults(json.results);
+        setIsLoading(false);
+        setInitialValue(query);
+        props.updateResults(json.results);
       })
-      .catch(() => {
-        this.setState({ isLoading: true });
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(true);
       });
-  }
-
-  handleValue = (value: string) => {
-    this.setState({ inputValue: value });
-    this.setInitialValue(value);
-    this.setState({ isLoading: false });
   };
 
-  getInitialValue(): string {
+  const handleValue = (value: string) => {
+    setInputValue(value);
+    setInitialValue(value);
+    setIsLoading(false);
+  };
+
+  const getInitialValue = (): string => {
     return localStorage.getItem('inputValue') || '';
-  }
+  };
 
-  setInitialValue(value): void {
+  const setInitialValue = (value): void => {
     return localStorage.setItem('inputValue', value);
-  }
+  };
 
-  componentDidMount(): void {
-    const value = this.getInitialValue();
-    this.setState({ ...this.state, inputValue: value });
-    this.fetchData(value);
-  }
-  handleError(event: React.MouseEvent<HTMLButtonElement>) {
-    this.setState({
-      ...this.state,
-      hasError: true,
-    });
+  useEffect(() => {
+    const value = getInitialValue();
+    setInputValue(value);
+    fetchData(value);
+  }, [setSearchTerm]);
+
+  const handleError = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setHasError(true);
     event.preventDefault();
-  }
+  };
 
-  render() {
-    if (this.state.hasError) throw new Error('Fallback');
-    return (
-      <div>
-        <button onClick={(e) => this.handleError(e)}>Make Error</button>
-        <div className="input-wrapper">
-          <FaSearch className="search-icon" />
-          <input
-            placeholder="Type to search"
-            value={this.state.inputValue}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              this.handleValue(e.target.value)
-            }
-          />
-          <button
-            onClick={() => this.fetchData(this.state.inputValue)}
-            disabled={this.state.isLoading}
-          >
-            Search
-          </button>
-        </div>
+  if (hasError) throw new Error('Fallback');
+
+  return (
+    <div>
+      <button onClick={(e) => handleError(e)}>Make Error</button>
+      <div className="input-wrapper">
+        <FaSearch className="search-icon" />
+        <input
+          placeholder="Type to search"
+          value={inputValue}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            handleValue(e.target.value)
+          }
+        />
+        <button onClick={() => fetchData(inputValue)} disabled={isLoading}>
+          Search
+        </button>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default SearchBar;
