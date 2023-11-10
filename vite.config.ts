4c56534457305988982041454ed28644c770cde1
Maryia Piarshai <mary.pershay@gmail.com>
2023-11-10 18:20:02 +0100

    feat: add test config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, './src'),
+      components: path.resolve(__dirname, './src/components'),
+    },
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+  },
 });
